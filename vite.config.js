3d5b28935d1982ce2d6cf452e63b4803742b1c04
bilsim-code <bildadsimiyu6@gmail.com>
2024-06-18 19:28:07 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/CarWeb-Landing-Page/",
+  build: {
+    outDir: "dist"
+  }
 })
